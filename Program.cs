734c468d9a7b565ba1ebd6273b4fc29cc2391a1f
DESKTOP-EESHTSG\new user <nikkit.zp@gmail.com>
2022-09-23 10:36:18 +0100

    Title searching function (case 9) moved to TitleSearch class

--- Program.cs
+++ Program.cs
@@ -56,54 +56,9 @@ namespace Project_3
                     case "8":
                         DescriptionAllBooks.Desription(library);
                         break;
+                    //Done
                     case "9":
-                        if (numOfBooks == 0)
-                        {
-                            Console.WriteLine("Your library is empty. Add books first.");
-                            PressAndContinue();
-                            continue;
-                        }
-                        while (exceptionChoice == false)
-                        {
-                            try
-                            {
-                                Console.WriteLine("Enter the title you want to find");
-                                string titleFind = Console.ReadLine();
-                                Console.Clear();
-                                if (titleFind == "")
-                                    Convert.ToInt32("");
-                                exceptionChoice = true;
-                                bool search = false;
-                                for (int i = 0; i < library.Length; i++)
-                                {
-                                    if (library[i].GetTitle() == titleFind)
-                                    {
-                                        library[i].GetBook(out string titleGet, out string authorGet, out int pagesGet);
-                                        Console.WriteLine("Book number " + (i + 1));
-                                        Console.WriteLine(titleGet);
-                                        Console.WriteLine("by " + authorGet);
-                                        Console.WriteLine(pagesGet + " pages");
-                                        Console.WriteLine();
-                                        search = true;
-                                    }
-                                }
-                                if (search == false)
-                                    Console.WriteLine("No books found");
-                            }
-                            catch (Exception)
-                            {
-                                Console.WriteLine("Invalid value has been entered.");
-                                PressAndContinue();
-                            }
-                            Console.WriteLine("Enter 1, if you want to search once more");
-                            Console.WriteLine("Enter anything else to return to the main menu");
-                            string choice2 = Console.ReadLine();
-                            Console.Clear();
-                            if (choice2 == "1")
-                                exceptionChoice = false;
-                            else
-                                exceptionChoice = true;
-                        }
+                        TitleSearch.Search(library);
                         break;
                     case "10":
                         if (numOfBooks != 0)
---
+++ TitleSearch.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_3
+{
+    class TitleSearch
+    {
+        static public void Search(Book[] library)
+        {
+            if (library.Length != 0)
+            {
+                bool exceptionChoice = false;
+                while (exceptionChoice == false)
+                {
+                    try
+                    {
+                        Console.WriteLine("Enter the title you want to find");
+                        string titleFind = Console.ReadLine();
+                        Console.Clear();
+                        if (titleFind == "")
+                            Convert.ToInt32("");
+                        exceptionChoice = true;
+                        bool search = false;
+                        for (int i = 0; i < library.Length; i++)
+                        {
+                            if (library[i].GetTitle() == titleFind)
+                            {
+                                library[i].GetBook(out string titleGet, out string authorGet, out int pagesGet);
+                                Console.WriteLine("Book number " + (i + 1));
+                                Console.WriteLine(titleGet);
+                                Console.WriteLine("by " + authorGet);
+                                Console.WriteLine(pagesGet + " pages");
+                                Console.WriteLine();
+                                search = true;
+                            }
+                        }
+                        if (search == false)
+                            Console.WriteLine("No books found");
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Invalid value has been entered.");
+                        Program.PressAndContinue();
+                    }
+                    Console.WriteLine("Enter 1, if you want to search once more");
+                    Console.WriteLine("Enter anything else to return to the main menu");
+                    string choice2 = Console.ReadLine();
+                    Console.Clear();
+                    if (choice2 == "1")
+                        exceptionChoice = false;
+                    else
+                        exceptionChoice = true;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Your library is empty. Add books first.");
+            }
+        }
+    }
+}
