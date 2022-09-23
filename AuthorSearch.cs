02e3ca63e3c8cc5ad748974f00fb30a662667403
DESKTOP-EESHTSG\new user <nikkit.zp@gmail.com>
2022-09-23 15:04:10 +0100

    Author searching function (case 10) moved to the AuthorSearch class.

---
+++ AuthorSearch.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_3
+{
+    class AuthorSearch
+    {
+        static public void Search(Book[] library)
+        {
+            if (library.Length != 0)
+            {
+                bool exceptionChoice = false;
+                while (exceptionChoice == false)
+                {
+                    Console.WriteLine("Enter the author you want to find");
+                    string authorFind = Console.ReadLine();
+                    Console.Clear();
+                    if (authorFind == "")
+                        authorFind = "No Author";
+                    exceptionChoice = true;
+                    bool search = false;
+                    for (int i = 0; i < library.Length; i++)
+                    {
+                        if (library[i].GetAuthor() == authorFind)
+                        {
+                            library[i].GetBook(out string titleGet, out string authorGet, out int pagesGet);
+                            Console.WriteLine("Book number " + (i + 1));
+                            Console.WriteLine(titleGet);
+                            Console.WriteLine("by " + authorGet);
+                            Console.WriteLine(pagesGet + " pages");
+                            Console.WriteLine();
+                            search = true;
+                        }
+                    }
+                    if (search == false)
+                    {
+                        Console.WriteLine("No books found");
+                    }
+                    Console.WriteLine("Enter 1, if you want to search once more");
+                    Console.WriteLine("Enter anything else to return to the main menu");
+                    string choice2 = Console.ReadLine();
+                    Console.Clear();
+                    if (choice2 == "1")
+                        exceptionChoice = false;
+                }
+            }
+            else
+                Console.WriteLine("Your library is empty. Add books first.");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -61,46 +61,7 @@ namespace Project_3
                         TitleSearch.Search(library);
                         break;
                     case "10":
-                        if (numOfBooks != 0)
-                        {
-                            while (exceptionChoice == false)
-                            {
-                                Console.WriteLine("Enter the author you want to find");
-                                string authorFind = Console.ReadLine();
-                                Console.Clear();
-                                if (authorFind == "")
-                                    authorFind = "No Author";
-                                exceptionChoice = true;
-                                bool search = false;
-                                for (int i = 0; i < library.Length; i++)
-                                {
-                                    if (library[i].GetAuthor() == authorFind)
-                                    {
-                                        library[i].GetBook(out string titleGet, out string authorGet, out int pagesGet);
-                                        Console.WriteLine("Book number " + (i + 1));
-                                        Console.WriteLine(titleGet);
-                                        Console.WriteLine("by " + authorGet);
-                                        Console.WriteLine(pagesGet + " pages");
-                                        Console.WriteLine();
-                                        search = true;
-                                    }
-                                }
-                                if (search == false)
-                                {
-                                    Console.WriteLine("No books found");
-                                }
-                                Console.WriteLine("Enter 1, if you want to search once more");
-                                Console.WriteLine("Enter anything else to return to the main menu");
-                                string choice2 = Console.ReadLine();
-                                Console.Clear();
-                                if (choice2 == "1")
-                                    exceptionChoice = false;
-                                else
-                                    alreadyDone = true;
-                            }
-                        }
-                        else
-                            Console.WriteLine("Your library is empty. Add books first.");
+                        AuthorSearch.Search(library);
                         break;
                     case "11":
                         if (numOfBooks != 0)
